96d48de62abe1ca580a0a34b8ec0d308a9d54984
Ayush Sunariya <69513008+ayush478@users.noreply.github.com>
2021-03-02 09:32:29 +0530

    map updates

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
 import './App.css';
 import InfoBox from './infoBox';
 import Map from './Map';
+import 'leaflet/dist/leaflet.css';
 import Table from './table';
 import { sortData } from './util';
 import LineGraph from './LineGraph';
@@ -19,6 +20,9 @@ function App() {
   const [country, setCountry] = useState('worldwide');
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(2);
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     fetch('https://disease.sh/v3/covid-19/all')
@@ -45,23 +49,35 @@ function App() {
 
     getCountriesData();
   }, []);
-
   const onCountryChange = async (event) => {
+    setLoading(true);
     const countryCode = event.target.value;
 
+    setCountry(countryCode);
+
     const url =
-      countryCode === 'worldwide'
-        ? 'https://disease.sh/v3/covid-19/all'
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
         : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    //https://disease.sh/v3/covid-19/all
+    //https://disease.sh/v3/covid-19/countries/[countryCode]
 
     await fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setCountry(countryCode);
         setCountryInfo(data);
+        setLoading(false);
+        // console.log([data.countryInfo.lat, data.countryInfo.long]);
+        countryCode === "worldwide"
+          ? setMapCenter([34.80746, -40.4796])
+          : setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        setMapZoom(4);
       });
+
+    console.log(countryInfo);
   };
-  console.log(countryInfo);
   return (
     <div className='app'>
       <div className='app__left'>
@@ -100,13 +116,13 @@ function App() {
             total={countryInfo.deaths}
           ></InfoBox>
         </div>
-        <Map />
+        <Map center={mapCenter} zoom={mapZoom} />
       </div>
       <Card className='app__right'>
         <CardContent>
           <h3>live cases by country</h3>
           <Table countries={tableData} />
-          <h3>worldwide new cases</h3>
+          <h3>Worldwide new cases</h3>
           <LineGraph />
         </CardContent>
       </Card>
